258fcd1a7117e1cb4a4f429beda60e890196c3cf
bhavin <jariwala24b@gmail.com>
2022-08-09 12:55:38 +0530

    modification q-7

--- q-7/app.js
+++ q-7/app.js
@@ -1,11 +1,24 @@
 const util = require('util');
 const fs = require('fs');
 
-const unlinkPromisify = util.promisify(fs.unlink);
+// 1) Using util module promisify function
+// const unlinkPromisify = util.promisify(fs.unlink);
+
+// 2) creating custom promise function
+const unlinkPromisify = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.unlink(path, (err) => {
+            if (err)
+                reject()
+            else
+                resolve()
+        })
+    })
+}
 
 const removeFile = async () => {
     try {
-        const response = await unlinkPromisify('./q-7/text.txt');
+        const response = await unlinkPromisify('./text.txt');
     }
     catch (err) {
         console.log(err);
